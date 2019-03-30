7b3aeec272958f4b136dbd02c09a10e45f5376b1
nico leputois <NicoLeputois@hotmail.com>
2019-03-30 18:59:51 +0100

    Ajoutez des fichiers projet.

---
+++ src/Collector.Common.FeatureFlags/DefaultValueFeatureFlagProvider.cs
@@ -0,0 +1,50 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DefaultValueFeatureFlagProvider.cs" company="Collector AB">
+//   Copyright © Collector AB. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Collector.Common.FeatureFlags
+{
+    public class DefaultValueFeatureFlagProvider : IFeatureFlagProvider
+    {
+        public bool IsFeatureEnabled<T>(User user, bool defaultValue = false) where T : FeatureFlag<bool>, new()
+        {
+            return defaultValue;
+        }
+
+        public bool IsFeatureEnabled<T>(bool defaultValue = false) where T : FeatureFlag<bool>, new()
+        {
+            return defaultValue;
+        }
+
+        public bool GetBoolFlag<T>(User user, bool defaultValue = false) where T : FeatureFlag<bool>, new()
+        {
+            return defaultValue;
+        }
+
+        public bool GetBoolFlag<T>(bool defaultValue = false) where T : FeatureFlag<bool>, new()
+        {
+            return defaultValue;
+        }
+
+        public int GetIntFlag<T>(User user, int defaultValue) where T : FeatureFlag<int>, new()
+        {
+            return defaultValue;
+        }
+
+        public int GetIntFlag<T>(int defaultValue) where T : FeatureFlag<int>, new()
+        {
+            return defaultValue;
+        }
+
+        public string GetStringFlag<T>(User user, string defaultValue) where T : FeatureFlag<string>, new()
+        {
+            return defaultValue;
+        }
+
+        public string GetStringFlag<T>(string defaultValue) where T : FeatureFlag<string>, new()
+        {
+            return defaultValue;
+        }
+    }
+}
---
+++ src/Collector.Common.FeatureFlags/User.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="User.cs" company="Collector AB">
+//   Copyright © Collector AB. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Collector.Common.FeatureFlags
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class User
+    {
+        private const string CANNOT_BE_NULL_OR_EMPTY = "Cannot be null or empty.";
+
+        public User(string teamKeyPart, string userKeyPart)
+        {
+            if (string.IsNullOrWhiteSpace(teamKeyPart))
+                throw new ArgumentException(CANNOT_BE_NULL_OR_EMPTY, nameof(teamKeyPart));
+
+            if (string.IsNullOrWhiteSpace(userKeyPart))
+                throw new ArgumentException(CANNOT_BE_NULL_OR_EMPTY, nameof(userKeyPart));
+
+            Key = $"{teamKeyPart}-{userKeyPart}";
+            CustomAttributes = new Dictionary<string, object>();
+        }
+
+        public string Key { get; }
+
+        public string Email { get; set; }
+
+        public string CountryCode { get; set; }
+
+        public IDictionary<string, object> CustomAttributes { get; }
+
+        public User With(string customAttributeName, object customAttributeValue)
+        {
+            CustomAttributes[customAttributeName] = customAttributeValue;
+
+            return this;
+        }
+    }
+}
